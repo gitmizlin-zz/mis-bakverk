d48cc475b99d6afdc97b7e4c93aac975e50175e5
mizlin <mizlin@gmail.com>
2016-02-15 15:55:34 +0100

    portion scale added. not working

--- src/script.js
+++ src/script.js
@@ -8,3 +8,11 @@ window.onload = myTest;
 function pic() {
 	document.getElementById("testImg").innerHTML = "kanelbullar är goda";
 }
+
+// hämta fler än ett element.
+document.getElementByTagName("li");
+
+function changePortion() {
+	var portions = document.getElementById("quantity").value;
+	alert("Portions: " + portions);
+}
