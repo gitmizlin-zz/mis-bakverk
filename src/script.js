7561ad0de994e59cfcfdd3f3e85c636cb1e3ee9e
mizlin <mizlin@gmail.com>
2016-02-15 15:09:36 +0100

    javascript file created

---
+++ src/script.js
@@ -0,0 +1,10 @@
+function myTest() {
+	document.getElementById("test").innerHTML = "sidan kapades!!";
+	console.log("testing...");
+}
+
+window.onload = myTest;
+
+function pic() {
+	document.getElementById("testImg").innerHTML = "kanelbullar är goda";
+}
