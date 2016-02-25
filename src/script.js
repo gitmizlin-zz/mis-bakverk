24072d7634f03205f6d21e4a10c7af5f0da8f66f
mizlin <mizlin@gmail.com>
2016-02-25 10:32:23 +0100

    portions local strage OK

--- src/script.js
+++ src/script.js
@@ -1,9 +1,14 @@
-var portions = 0;
+var portions;
 var myPoint = 0;
 var isRated = false;
 
-function changePortion() {
-	portions = document.getElementById("quantity").value;
+$('#votes').text(getLocalStorage("key2"));
+$('#average').text(getLocalStorage("key3"));
+
+
+function displayPortion() {
+	portions = getLocalStorage("key1");
+	displaySlider();
 	var text = "person";
 	if (portions == 1) {
 		text = text;
@@ -18,7 +23,17 @@ function changePortion() {
 	cream();
 	vanilla();
 }
-window.onload = changePortion;
+window.onload = displayPortion;
+
+function displaySlider() {
+	$("#quantity").val(portions);
+}
+
+function changePortion() {
+	portions = document.getElementById("quantity").value;
+	setLocalStorage("key1", portions);
+	displayPortion();
+}
 
 function eggYolk() {
 	var text = "egg yolk";
@@ -54,7 +69,6 @@ function vanilla() {
 }
 
 $('.ratingForm input').click(function() {
-	console.log("clicked");
 	$.ajax({
 		method: "GET",
 		url: "https://edu.oscarb.se/sjk15/api/recipe/?api_key=984d3fec6c2e1f94&recipe=creme_brulee",
@@ -62,6 +76,8 @@ $('.ratingForm input').click(function() {
 			console.log(JSON.stringify(data));
 			$('#votes').text(data.votes);
 			$('#average').text(data.rating.toFixed(1));
+			setLocalStorage("key2", data.votes);
+			setLocalStorage("key3", data.rating.toFixed(1));
 		},
 		error: function(jqXHR, textStatus, errorThrown) {
           console.log(textStatus, errorThrown);
@@ -70,15 +86,14 @@ $('.ratingForm input').click(function() {
 });
 
 $('.ratingForm input').click(function() {
-	console.log("clicked");
-	var user_rating = ($('input[name=rating]:checked', '.ratingForm').val());
+	myPoint = ($('input[name=rating]:checked', '.ratingForm').val());
 	$.ajax({
 		method: "GET",
-		url: "https://edu.oscarb.se/sjk15/api/recipe/?api_key=984d3fec6c2e1f94&recipe=creme_brulee&rating=" + user_rating,
+		url: "https://edu.oscarb.se/sjk15/api/recipe/?api_key=984d3fec6c2e1f94&recipe=creme_brulee&rating=" + myPoint,
 		success: function(data) {
 			console.log(JSON.stringify(data));
 			console.log("status: " + data.status);
-			$('#myRating').text(user_rating);
+			$('#myRating').text(myPoint);
             $(':radio:not(:checked)').attr('disabled', true);
             isRated = true;
 		},
@@ -92,31 +107,35 @@ $('.ratingForm label').hover(function() {
 	if (!isRated) {
 		var	value = ($('input[name=rating]:hover', '.ratingForm').val());
 		var i = 0;
-		console.log("hovered");
-		var f = $(this).attr("for");
-
-		while (i <= value) {
+				while (i <= value) {
 			$('label[for=star' + i + ']').css('backgroundImage', "url('../img/star_pink.png')");
+
 			i++;
 		}
 	}
 }, function() {
 	if (!isRated) {
 		$('.ratingForm label').css('backgroundImage', "url('../img/star_grey.png')");
-		console.log("not hovered" + $(this).attr("for"));
-		var f = $(this).attr("for");
+//		var f = $(this).attr("for");
 	}
 });
 
-function CheckBrowser() {
-    if ('localStorage' in window && window['localStorage'] !== null) {
-        // we can use localStorage object to store data
-        console.log("local storage supported");
-        return true;
-    } else {
-     	console.log("local storage not supported");
-        return false;
-    }
+function getLocalStorage(key) {
+	if(typeof(window.localStorage) != 'undefined'){
+		portions = window.localStorage.getItem(key);
+	} else {
+		throw "window.localStorage, not defined";
+	}
+	return portions;
+}
+
+function setLocalStorage(key, value) {
+	if(typeof(window.localStorage) != 'undefined'){
+		window.localStorage.setItem(key, value);
+	}
+	else{
+		throw "window.localStorage, not defined";
+	}
 }
 
 
